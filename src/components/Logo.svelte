f6df2d17ffae9daf89d309d90dc447d732d8d806
d4niel <olayajuandaniel8@gmail.com>
2023-11-05 17:18:16 -0500

    inputs

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,3 +1,6 @@
-<div class="navbar bg-base-300">
+<div class="navbar">
     <a class="btn btn-ghost normal-case text-xl">Sql Exchange</a>
+</div>
+<div>
+    <a class="btn btn-ghost normal-case hover:none">Translate your Sql Sentences</a>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
     import Logo from '../components/Logo.svelte'
+    import Translate from '../components/Translate.svelte';
 </script>
 
-<Logo/>
+<main>
+    <div class="max-w-4xl mx-auto px-4">
+        <Logo/>
+        <Translate/>
+    </div>
+</main>
