f863131e98a791ef6f3cf2ee3962ec04bb29ed1c
d4niel <olayajuandaniel8@gmail.com>
2023-11-05 17:25:04 -0500

    layout

--- src/components/Translate.svelte
+++ src/components/Translate.svelte
@@ -3,15 +3,15 @@
 
 <div class="flex w-full items-center justify-between">
     <div class="w-full p-2">
-        <select class="select select-bordered w-full max-w-xs m-4">
+        <select class="select select-bordered  w-full max-w-xs m-4">
             <option disabled selected>Provider</option>
             <option>Han Solo</option>
             <option>Greedo</option>
           </select>
-        <textarea class="textarea textarea-bordered w-full mt-4"></textarea>
+        <textarea class="textarea textarea-bordered  w-full h-56 mt-4"></textarea>
     </div>
     <div>
-        <button class="btn btn-neutral">Translate</button>
+        <button class="btn btn-primary">Translate</button>
     </div>
     <div class="w-full p-2">
         <select class="select select-bordered w-full max-w-xs  m-4">
@@ -19,6 +19,6 @@
             <option>Han Solo</option>
             <option>Greedo</option>
           </select>
-        <textarea class="textarea textarea-bordered w-full mt-4"></textarea>
+        <textarea class="textarea textarea-bordered w-full h-56 mt-4"></textarea>
     </div>
 </div>
