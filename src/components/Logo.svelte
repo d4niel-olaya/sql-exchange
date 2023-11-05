348a2767521eddbdd06004a6cdbe811579f3e9fb
d4niel <olayajuandaniel8@gmail.com>
2023-11-05 17:30:01 -0500

    select

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -2,5 +2,5 @@
     <a class="btn btn-ghost normal-case text-xl">Sql Exchange</a>
 </div>
 <div>
-    <a class="btn btn-ghost normal-case hover:none">Translate your Sql Sentences</a>
+    <a class="btn btn-ghost normal-case hover:none">Translate your SQL code from different database management systems to a specific one</a>
 </div>
--- src/components/Translate.svelte
+++ src/components/Translate.svelte
@@ -5,8 +5,10 @@
     <div class="w-full p-2">
         <select class="select select-bordered  w-full max-w-xs m-4">
             <option disabled selected>Provider</option>
-            <option>Han Solo</option>
-            <option>Greedo</option>
+            <option>MySql</option>
+            <option>Postgres</option>
+            <option>Oracle</option>
+            <option>SQL Server</option>
           </select>
         <textarea class="textarea textarea-bordered  w-full h-56 mt-4"></textarea>
     </div>
@@ -16,8 +18,10 @@
     <div class="w-full p-2">
         <select class="select select-bordered w-full max-w-xs  m-4">
             <option disabled selected>Provider</option>
-            <option>Han Solo</option>
-            <option>Greedo</option>
+            <option>MySql</option>
+            <option>Postgres</option>
+            <option>Oracle</option>
+            <option>SQL Server</option>
           </select>
         <textarea class="textarea textarea-bordered w-full h-56 mt-4"></textarea>
     </div>
