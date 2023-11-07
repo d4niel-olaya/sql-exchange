aa19ab920d4a63a8f01a225fec8de036558112d8
d4niel <olayajuandaniel8@gmail.com>
2023-11-06 21:48:16 -0500

    hero

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -12,5 +12,5 @@
 </div>
 <div>
     <!-- svelte-ignore a11y-missing-attribute -->
-    <a class="btn btn-ghost normal-case hover:none">Translate your SQL code from different database management systems to a specific one</a>
+    <a class="btn btn-ghost normal-case hover:none">Translate SQL code from diverse database systems to a single target system.</a>
 </div>
